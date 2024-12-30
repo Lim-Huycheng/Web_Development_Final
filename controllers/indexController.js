47da04ce09c08868795c62b89de9d00b1ec7a5de
lim huycheng <limhuycheng23@gmail.com>
2024-12-30 22:26:46 +0700

    work

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,11 +1,9 @@
 const Product = require("../models/productModel");
 
-
-
 exports.getListProduct = async (req,res) => {
     try{
         const products = await Product.getAll();
-        title = "Homepage";
+        title = "List Product";
         res.render('index/index',{products ,title});
     
     }catch(err){
--- controllers/productController.js
+++ controllers/productController.js
@@ -5,7 +5,7 @@ exports.getAllProducts = async (req,res) => {
     try{
         const products = await Product.getAll();
         title = "List product"
-        res.render('index/index',{products,title});
+        res.render('product/index',{products,title});
         
     }catch(err){
         res.status(500).send("Error fetching products");
