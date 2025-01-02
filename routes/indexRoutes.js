4176fca595b861e78e64b9d635d5fed48ee69a8e
Keo-Sokanya <keosokanya18@gmail.com>
2025-01-02 21:15:32 +0700

    new

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -11,7 +11,7 @@ router.use(methodOverride('_method'));
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'root',
+  password: '123',
   database: 'gproducts'
 });
 
